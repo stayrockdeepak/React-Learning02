82cfc2979fa4c67a38d219fd95fb2c086a551d79
unknown <you@example.com>
2025-01-13 15:38:15 +0530

    React Episode - 01

---
+++ App.js
@@ -0,0 +1,14 @@
+const haeding = React.createElement(
+    "h1",
+    {id: "heading"}, 
+    "Hello world from React"
+);
+
+const parent = React.createElement("div", {id:'parent'}, 
+    [React.createElement("div", {id:'child'}, 
+    [React.createElement("h1", {}, "I am h1 tag"),React.createElement("h2", {}, "I am h2 tag")]),
+    React.createElement("div", {id:'child2'}, [React.createElement("h1", {}, "I am h1 tag"),React.createElement("h2", {}, "I am h2 tag")])])
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent)
